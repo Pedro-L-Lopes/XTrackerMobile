92b0871ad6da5734dc718af22fb0e922915b6ec9
Pedro Lucas <pedrolucas122737@gmail.com>
2024-04-11 09:43:50 -0300

    feat: new habit Checkbox

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
+
+interface Props extends TouchableOpacityProps {
+  title: string;
+  checked?: boolean;
+}
+
+export function Checkbox({ title, checked = false, ...rest }: Props) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className="flex-row mb-2 items-center"
+      {...rest}
+    >
+      {checked ? (
+        <View className="w-10 h-10 rounded-lg bg-green-600 items-center justify-center">
+          <Feather name="check" size={20} color={colors.white} />
+        </View>
+      ) : (
+        <View className="h-10 w-10 bg-zinc-800 rounded-lg" />
+      )}
+
+      <Text className="text-white text-base ml-3">{title}</Text>
+    </TouchableOpacity>
+  );
+}
--- src/screens/NewHabit.tsx
+++ src/screens/NewHabit.tsx
@@ -1,7 +1,31 @@
 import { View, Text, ScrollView, TextInput } from "react-native";
 import { BackButton } from "../components/Backbutton";
+import { Checkbox } from "../components/Checkbox";
+import { useState } from "react";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function NewHabit() {
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState("");
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) =>
+        prevState.filter((weekDay) => weekDay !== weekDayIndex)
+      );
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  }
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -16,6 +40,23 @@ export function NewHabit() {
         </Text>
 
         <TextInput className="h-14 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 border-green-600" />
+
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
+          Qual a recorrência?
+        </Text>
+
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox
+            key={weekDay}
+            title={weekDay}
+            checked={weekDays.includes(index)}
+            onPress={() => handleToggleWeekDay(index)}
+          />
+        ))}
+
+        {/* <Text className="font-semibold text-base text-white ml-2">
+          Confirmar
+        </Text> */}
       </ScrollView>
     </View>
   );
