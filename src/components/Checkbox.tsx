140b784169c50972ae0278448711acf9edfcdf8c
Pedro Lucas <pedrolucas122737@gmail.com>
2024-04-14 16:47:17 -0300

    feat: checkbox and progressbar animation

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import {
-  View,
   Text,
   TouchableOpacity,
   TouchableOpacityProps,
+  View,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 
 interface Props extends TouchableOpacityProps {
   title: string;
@@ -21,11 +21,15 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
       {...rest}
     >
       {checked ? (
-        <View className="w-10 h-10 rounded-lg bg-green-600 items-center justify-center">
+        <Animated.View
+          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+          entering={ZoomIn}
+          exiting={ZoomOut}
+        >
           <Feather name="check" size={20} color={colors.white} />
-        </View>
+        </Animated.View>
       ) : (
-        <View className="h-10 w-10 bg-zinc-900 rounded-lg border border-zinc-800" />
+        <View className="h-8 w-8 bg-zinc-900 rounded-lg border border-zinc-800" />
       )}
 
       <Text className="text-white text-base ml-3 font-semibold">{title}</Text>
